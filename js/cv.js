c2ef6a3becbad3ee0467cb57efae14744c4bb00c
EoinFarrell <efarrell@zendesk.com>
2020-01-01 19:50:50 +0000

    Clearing down as this will now be pushed to dynamically

--- js/cv.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function (){
-    $.ajax({
-        url: "http://localhost:5000/api/values"
-    }).done(function(data){
-        cv = JSON.parse(data);
-        $("#name").text(cv.name);
-    })
-})
